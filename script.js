66e8b28bfe11e911f7e687e0d5fd18c166305014
Anne Larimer <aflarimer@crimson.ua.edu>
2022-02-01 11:26:18 -0600

    final commit

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ function handleOnSearch() {
 }
 
 function apiSearch() {
-  var params = {
+    var results = '';
+    var params = {
     "q": $("#query").val(),
     "count": "50",
     "offset": "0",
@@ -32,7 +33,7 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'https://management.azure.com/v7.0/search?' + $.param(params),
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "e64bd68742594f67a12d1412bf3aee0a");
       },
@@ -46,6 +47,7 @@ function apiSearch() {
 
       $('#searchResults').html(results);
       $('#searchResults').dialog();
+
     })
     .fail(function () {
       alert("error");
